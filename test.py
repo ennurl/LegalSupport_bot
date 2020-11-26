2dadc04ca775a3210becddf687d58fe95fdfe667
ennurl <60938572+ennurl@users.noreply.github.com>
2020-11-26 12:51:00 +0300

    pupa

--- test.py
+++ test.py
@@ -2,6 +2,10 @@
 import telebot
 
 bot = telebot.TeleBot('1495506119:AAF_ZyrAlBSU4_9anJKHLVMBukjB2YAzC50')
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'привет привет ')
+
+
+bot.polling()
